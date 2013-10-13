38b7b6fd3518eb50bd344b8cd4d13c20224f1bad
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-10-14 00:44:55 +0200

    Prepare propel connection for rentgen

--- DependencyInjection/CzogoriDamiExtension.php
+++ DependencyInjection/CzogoriDamiExtension.php
@@ -3,19 +3,37 @@
 namespace Czogori\DamiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class CzogoriDamiExtension extends Extension
+class CzogoriDamiExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
-    {
+    {    
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function prepend(ContainerBuilder $container)
+	{		
+ 		$configs = $container->getExtensionConfig('propel'); 		
+ 		if(isset($configs[0]['dbal'])) {
+ 			$dbal = $configs[0]['dbal'];
+
+	 		$config['adapter'] = $dbal['driver'];
+	 		$config['username'] = $dbal['user'];
+	 		$config['password'] = $dbal['password'];
+	 		$config['dsn'] = $dbal['dsn'];
+	        $container->prependExtensionConfig('rentgen', $config);
+	    }
+	}
 }
