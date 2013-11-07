74dfa7baa55578eafd457ac7c6efe6ec1f5345fd
Arek Jaskólski <arek.jaskolski@gmail.com>
2013-11-07 23:28:27 +0100

    Create migration in specific bundle

--- Command/CreateCommand.php
+++ Command/CreateCommand.php
@@ -4,9 +4,10 @@ namespace Czogori\DamiBundle\Command;
 
 use Symfony\Component\Console\Input\InputArgument,
     Symfony\Component\Console\Input\InputInterface,
-    Symfony\Component\Console\Output\OutputInterface;
+    Symfony\Component\Console\Output\OutputInterface,
+    Symfony\Component\Filesystem\Filesystem;
 
-use Dami\Cli\Command\CreateCommand as DamiCreateCommand;
+use Dami\Migration\FileNameBuilder;
 
 class CreateCommand extends AbstractCommand
 {
@@ -17,14 +18,34 @@ class CreateCommand extends AbstractCommand
             ->setDescription('Create a new migration.')
             ->setDefinition(array(
                 new InputArgument('migration_name', InputArgument::REQUIRED, 'Migration name'),
+                new InputArgument('bundle_name', InputArgument::REQUIRED, 'Bundle name'),   
         ));
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->prepareMigrationDirectory();
+        $arguments = $input->getArguments();
+        $migrationName = $arguments['migration_name'];
+        $bundleName = $arguments['bundle_name'];
 
-        $damiStatusCommand = new DamiCreateCommand($this->getName(), $this->getContainer());
-        $damiStatusCommand->execute($input, $output);
+        $kernel = $this->getContainer()->get('kernel');
+        foreach ($kernel->getBundles() as $bundle) {
+            if($bundle->getName() === $bundleName) {
+                echo $bundle->getName();
+            
+                $bundleMigrationDirectory = $bundle->getPath() . '/Resources/migrations';
+                if (file_exists($bundleMigrationDirectory)) {
+                    $filenameBuilder = new FileNameBuilder($migrationName);
+                    $fileName = $filenameBuilder->build();
+
+                    $templateRenderer = $this->getContainer()->get('template_renderer');                    
+                    $path = $bundleMigrationDirectory . '/' . $fileName;
+                    
+                    $fileSystem = new Filesystem();
+                    $fileSystem->dumpFile($path, $templateRenderer->render($migrationName));
+                }            
+                break;
+            }
+        }       
     }
 }
