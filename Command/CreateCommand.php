365f7e7a5d78588e648d683260dc78336336c4f9
Arek Jaskólski <arek.jaskolski@gmail.com>
2014-01-17 00:57:36 +0100

    Added information when migration is creating

--- Command/CreateCommand.php
+++ Command/CreateCommand.php
@@ -29,21 +29,31 @@ class CreateCommand extends AbstractCommand
         $bundleName = $arguments['bundle_name'];
 
         $kernel = $this->getContainer()->get('kernel');
+        $budleExists = false;
         foreach ($kernel->getBundles() as $bundle) {
             if ($bundle->getName() === $bundleName) {
                 $bundleMigrationDirectory = $bundle->getPath() . '/Resources/migrations';
                 if (file_exists($bundleMigrationDirectory)) {
                     $filenameBuilder = new FileNameBuilder($migrationName);
-                    $fileName = $filenameBuilder->build();
-
-                    $templateRenderer = $this->getContainer()->get('template_renderer');
-                    $path = $bundleMigrationDirectory . '/' . $fileName;
-
                     $fileSystem = new Filesystem();
-                    $fileSystem->dumpFile($path, $templateRenderer->render($migrationName));
+                    $templateRenderer = $this->getContainer()->get('template_renderer');
+                    try {
+                        $fileName = $filenameBuilder->build();
+                        $path = $bundleMigrationDirectory . '/' . $fileName;
+                        $fileSystem->dumpFile($path, $templateRenderer->render($migrationName));
+
+                        $output->writeln('<info>Migration has been created.</info>');
+                        $output->writeln(sprintf('<comment>Location: %s</comment>', $path));
+                    } catch (\Exception $e) {
+                        $output->writeln(sprintf("<error>Something went wrong.</error>\n\n%s", $e->getMessage()));
+                    }
                 }
+                $budleExists = true;
                 break;
             }
+        }
+        if(false === $budleExists) {
+            $output->writeln(sprintf('<comment>Bundle %s does not exist.</comment>', $bundleName));
         }
     }
 }
